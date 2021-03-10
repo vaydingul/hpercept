04d5476ac4de61a8436f7cc44b18b3412f5f7940
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-03-11 02:40:14 +0300

    Julia -> Python transformation

---
+++ hpercept/data_handle/phac.py
@@ -0,0 +1,38 @@
+class PHAC2:
+    """
+
+
+    """
+
+    def __init__(self,
+                 accelerometer=None,
+                 electrode_0=None,
+                 pac_0=None,
+                 dc_0=None,
+                 tac_0=None,
+                 tdc_0=None,
+                 electrode_1=None,
+                 pac_1=None,
+                 pdc_1=None,
+                 tac_1=None,
+                 tdc_1=None,
+                 controller_detail_state=None,
+                 image=None,
+                 adjective=None):
+
+        self.accelerometer = accelerometer
+        self.electrode_0 = electrode_0
+        self.pac_0 = pac_0
+        self.dc_0 = dc_0
+        self.tac_0 = tac_0
+        self.tdc_0 = tdc_0
+        self.electrode_1 = electrode_1
+        self.pac_1 = pac_1
+        self.pdc_1 = pdc_1
+        self.tac_1 = tac_1
+        self.tdc_1 = tdc_1
+        self.controller_detail_state = controller_detail_state
+        self.image = image
+        self.adjective = adjective
+
+
---
+++ hpercept/data_handle/utils.py
@@ -0,0 +1,169 @@
+import h5py
+import numpy as np
+from . import PHAC2
+from sklearn.decomposition import PCA
+from scipy.signal import resample
+
+EP_DICT = {"hold": "HOLD_FOR_10_SECONDS",
+		   "squeeze": "SQUEEZE_SET_PRESSURE_SLOW",
+		   "slow_slide": "SLIDE_5CM",
+		   "fast_slide": "MOVE_DOWN_5CM"}
+
+
+def fetch_instances(fn, adj_set):
+
+    # Open .hdf5 file to read content
+    file = h5py.File(fn, "r")
+    adjectives = file[adj_set].keys()
+    dir_set = []
+    counter = 0
+
+    for adjective in adjectives:
+        materials = file[adj_set][adjective].keys()
+
+        for material in materials:
+
+            dir_set.append(("/".join([adj_set, adjective, material]), counter))
+
+        counter += 1
+
+    return dir_set
+
+
+def open_instance(instance, fn):
+
+    # Open .hdf5 file to read content
+    file = h5py.File(fn, "r")
+
+    X = PHAC2(
+        np.array(file[instance]["accelerometer"]),
+        np.array(file[instance]["biotacs/finger_0/electrodes"]),
+        np.array(file[instance]["biotacs/finger_0/pac"]),
+        np.array(file[instance]["biotacs/finger_0/pdc"]),
+        np.array(file[instance]["biotacs/finger_0/tac"]),
+        np.array(file[instance]["biotacs/finger_0/tdc"]),
+        np.array(file[instance]["biotacs/finger_1/electrodes"]),
+        np.array(file[instance]["biotacs/finger_1/pac"]),
+        np.array(file[instance]["biotacs/finger_1/pdc"]),
+        np.array(file[instance]["biotacs/finger_1/tac"]),
+        np.array(file[instance]["biotacs/finger_1/tdc"]),
+        np.array(file[instance]["state/controller_detail_state"]),
+        None,
+        np.array(file[instance]["adjectives"]),
+    )
+
+    return X
+
+
+def preprocess_instance(X, y, fixed_length=150):
+
+    X_out = []
+
+    for x in X:
+
+        x.electrode_0 = (x.electrode_0 - np.mean(x.electrode_0, axis=2)) / \
+            np.std(x.electrode_0, axis=2)
+
+        x.pac_0 = (x.pac_0 - np.mean(x.pac_0, axis=1)) / \
+                   np.std(x.pac_0, axis=1)
+
+        x.pac_0 = np.mean(x.pac_0, axis=1)
+
+        x.pdc_0 = (x.pdc_0 - np.mean(x.pdc_0, axis=1)) / \
+                   np.std(x.pdc_0, axis=1)
+
+        x.tac_0 = (x.tac_0 - np.mean(x.tac_0, axis=1)) / \
+                   np.std(x.tac_0, axis=1)
+
+        x.tdc_0 = (x.tdc_0 - np.mean(x.tdc_0, axis=1)) / \
+                   np.std(x.tdc_0, axis=1)
+
+        pca = PCA(n_components=4)
+        x.electrode_0 = pca.fit(x.electrode_0)
+
+        # M = fit(PCA, x.electrode_0, maxoutdim=4, pratio=1.0)
+        # x.electrode_0 = transform(M, x.electrode_0)
+
+        x.electrode_1 = (x.electrode_1 - np.mean(x.electrode_1, axis=2)) / \
+            np.std(x.electrode_1, axis=2)
+
+        x.pac_1 = (x.pac_1 - np.mean(x.pac_1, axis=1)) / \
+                   np.std(x.pac_1, axis=1)
+
+        x.pac_1 = np.mean(x.pac_1, axis=1)
+
+        x.pdc_1 = (x.pdc_1 - np.mean(x.pdc_1, axis=1)) / \
+                   np.std(x.pdc_1, axis=1)
+
+        x.tac_1 = (x.tac_1 - np.mean(x.tac_1, axis=1)) / \
+                   np.std(x.tac_1, axis=1)
+
+        x.tdc_1 = (x.tdc_1 - np.mean(x.tdc_1, axis=1)) / \
+                   np.std(x.tdc_1, axis=1)
+
+        pca = PCA(n_components=4)
+        x.electrode_1 = pca.fit(x.electrode_1)
+
+        hold_ixs = x.controller_detail_state == EP_DICT["hold"]
+        squeeze_ixs = x.controller_detail_state == EP_DICT["squeeze"]
+        slow_slide_ixs = x.controller_detail_state == EP_DICT["slow_slide"]
+        fast_slide_ixs = x.controller_detail_state == EP_DICT["fast_slide"]
+
+        img = np.vstack(
+                [
+                    resample(x.pac_0[hold_ixs].T, fixed_length),
+                    resample(x.pdc_0[hold_ixs].T, fixed_length),
+                    resample(x.tac_0[hold_ixs].T, fixed_length),
+                    resample(x.tdc_0[hold_ixs].T, fixed_length),
+                    resample(x.electrode_0[:, hold_ixs], fixed_length),
+                    resample(x.pac_1[hold_ixs].T, fixed_length),
+                    resample(x.pdc_1[hold_ixs].T, fixed_length),
+                    resample(x.tac_1[hold_ixs].T, fixed_length),
+                    resample(x.tdc_1[hold_ixs].T, fixed_length),
+                    resample(x.electrode_1[:, hold_ixs], fixed_length),
+                    resample(x.pac_0[squeeze_ixs].T, fixed_length),
+                    resample(x.pdc_0[squeeze_ixs].T, fixed_length),
+                    resample(x.tac_0[squeeze_ixs].T, fixed_length),
+                    resample(x.tdc_0[squeeze_ixs].T, fixed_length),
+                    resample(x.electrode_0[:, squeeze_ixs], fixed_length),
+                    resample(x.pac_1[squeeze_ixs].T, fixed_length),
+                    resample(x.pdc_1[squeeze_ixs].T, fixed_length),
+                    resample(x.tac_1[squeeze_ixs].T, fixed_length),
+                    resample(x.tdc_1[squeeze_ixs].T, fixed_length),
+                    resample(x.electrode_1[:, squeeze_ixs], fixed_length),
+                    resample(x.pac_0[slow_slide_ixs].T, fixed_length),
+                    resample(x.pdc_0[slow_slide_ixs].T, fixed_length),
+                    resample(x.tac_0[slow_slide_ixs].T, fixed_length),
+                    resample(x.tdc_0[slow_slide_ixs].T, fixed_length),
+                    resample(x.electrode_0[:, slow_slide_ixs], fixed_length),
+                    resample(x.pac_1[slow_slide_ixs].T, fixed_length),
+                    resample(x.pdc_1[slow_slide_ixs].T, fixed_length),
+                    resample(x.tac_1[slow_slide_ixs].T, fixed_length),
+                    resample(x.tdc_1[slow_slide_ixs].T, fixed_length),
+                    resample(x.electrode_1[:, slow_slide_ixs], fixed_length),
+                    resample(x.pac_0[fast_slide_ixs].T, fixed_length),
+                    resample(x.pdc_0[fast_slide_ixs].T, fixed_length),
+                    resample(x.tac_0[fast_slide_ixs].T, fixed_length),
+                    resample(x.tdc_0[fast_slide_ixs].T, fixed_length),
+                    resample(x.electrode_0[:, fast_slide_ixs], fixed_length),
+                    resample(x.pac_1[fast_slide_ixs].T, fixed_length),
+                    resample(x.pdc_1[fast_slide_ixs].T, fixed_length),
+                    resample(x.tac_1[fast_slide_ixs].T, fixed_length),
+                    resample(x.tdc_1[fast_slide_ixs].T, fixed_length),
+                    resample(x.electrode_1[:, fast_slide_ixs], fixed_length),
+                ]
+        )
+
+        img = np.hstack(*img).T
+        if np.sum(np.isnan(img)) != 0:
+
+            img[np.isnan(img)] = np.mean(img[not np.isnan(img)])
+
+        
+
+        x.image = img
+
+    
+
+    return X, y
+
